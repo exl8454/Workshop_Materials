633248844dcc0e960fed82182978f2d32b6a6f49
Charlie S. Aka CS <hsmchul@hotmail.com>
2017-07-03 18:07:17 -0500

    Changed Library for Example_03

--- Arduino_Workshop_Files/Examples/Example_03/Example_03.ino
+++ Arduino_Workshop_Files/Examples/Example_03/Example_03.ino
@@ -5,31 +5,18 @@
  * such as computer
  */
 
-#include <Workshop_IMU.h>
+#include <LSM303_L3GD20H.h>
 
 void setup()
 {
   Serial.begin(115200);
   startSensor();
-
-  if(sensor_status != 0b11)
-  {
-    if(sensor_status == 0b10)
-      Serial.println("No Accelerometer!");
-    if(sensor_status == 0b01)
-      Serial.println("No Gyroscope!");
-    while(1);
-  }
-
-  Serial.println("Staring...");
-  delay(1000);
 }
 
 void loop()
 {
-  readSensors();
-  //Serial.print("Accl: "); Serial.print(a[0]); Serial.print(" "); Serial.print(a[1]); Serial.print(" "); Serial.println(a[2]); 
-  Serial.print("Gyro: "); Serial.print(g[0]); Serial.print(" "); Serial.print(g[1]); Serial.print(" "); Serial.println(g[2]);
-  delay(100);
+  readSensor();
+  Serial.print(m[0]); Serial.print(" "); Serial.print(m[1]); Serial.print(" "); Serial.print(m[2]); Serial.println();
+  delay(1000);
 }
 
